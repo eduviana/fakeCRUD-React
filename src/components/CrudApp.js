33af359f0ee9ba753b19726c8b4a2682b6e06f95
Eduardo Viana <eduardoviana83@gmail.com>
2021-09-01 19:07:04 -0300

    first commit

---
+++ src/components/CrudApp.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { CrudForm } from "./CrudForm";
+import CrudTable from "./CrudTable";
+
+const initialDb = [
+  {
+    id: 1,
+    name: "Seiya",
+    constellation: "Pegaso",
+  },
+  {
+    id: 2,
+    name: "Shiryu",
+    constellation: "Dragon",
+  },
+  {
+    id: 3,
+    name: "Hyoga",
+    constellation: "Cisne",
+  },
+  {
+    id: 4,
+    name: "Shun",
+    constellation: "Andromeda",
+  },
+  {
+    id: 5,
+    name: "Ikki",
+    constellation: "Fénix",
+  },
+];
+
+const CrudApp = () => {
+  const [db, setDb] = useState(initialDb);
+  const [dataToEdit, setDataToEdit] = useState(null);
+
+  const createData = (data) => {
+    data.id = Date.now();
+    setDb([...db, data]);
+  };
+
+  const updateData = (data) => {
+    let newData = db.map((el) => (el.id === data.id ? data : el));
+    setDb(newData);
+  };
+
+  const deleteData = (id) => {
+    let isDelete = window.confirm(
+      `¿Estás seguro de eliminar el registro con el id:'${id}'?`
+    );
+    if (isDelete) {
+      let newData = db.filter((el) => el.id !== id);
+      setDb(newData);
+    } else {
+    }
+  };
+
+  return (
+    <div>
+      <h2>CRUD App</h2>
+      <article className="grid-1-2">
+        <CrudForm
+          createData={createData}
+          updateData={updateData}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+
+        {/* Dentro del componente CrudTable se van a crear tantos CrudTableRow como elementos tenga la initialDb por medio del metodo map.
+      Cuando clickeemos en un boton dentro de CrudTableRow, éste va a propagar los datos al CrudTable y éste al CrudForm. */}
+        <CrudTable
+          data={db}
+          setDataToEdit={setDataToEdit}
+          deleteData={deleteData}
+        />
+      </article>
+    </div>
+  );
+};
+
+export default CrudApp;
